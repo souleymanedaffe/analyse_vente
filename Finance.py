6ed06871302f77670c785cee84c9d317bad4277a
souleymanedaffe <souleymane.daffe@arep.fr>
2025-08-01 17:28:57 +0200

    Update Finance.py

--- Finance.py
+++ Finance.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 import streamlit as st
-import matplotlib.pyplot as plt
+
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.preprocessing import LabelEncoder
